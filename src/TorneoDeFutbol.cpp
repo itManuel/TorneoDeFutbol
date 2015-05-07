0654427e42c1305cc5aea200c72c0cd3743b9890
Manuel Aller <manuel.aller@globant.com>
2015-05-07 16:23:00 -0300

    Cambios en la indentación, y saqué el mensaje de agradecimiento fuera del while.

--- src/TorneoDeFutbol.cpp
+++ src/TorneoDeFutbol.cpp
@@ -21,36 +21,38 @@ int main() {
 	Equipo equipo;
 	int opcion=1;
 	while(opcion!=0){
-	cout << "============================================================================" << endl;
-	cout << "#                         Nunca Viste Un Torneo Así                        #" << endl;
-	cout << "#                                                                          #" << endl;
-	cout << "#              Menu                                                        #" << endl;
-	cout << "#              1. Agregar Equipo                                           #" << endl;
-	cout << "#              2. Eliminar Equipo                                          #" << endl;
-	cout << "#              3. Ver Equipos                                              #" << endl;
-	cout << "#              0. Salir                                                    #" << endl;
-	cout << "#                                                                          #" << endl;
-	cout << "#                                                            grupo114      #" << endl;
-	cout << "============================================================================" << endl;
-	cin >> opcion;
-	switch (opcion){
-		case 1:
-			cout << "Esto hará que entres en el menú de agregar equipo" << endl;
-			break;
-		case 2:
-			cout << "Esto hará que entres en el menú de eliminar equipo" << endl;
-			break;
-		case 3:
-			cout << "Esto hará que entres en el menú de ver equipos" << endl;
-			break;
-		case 0:
-			cout << "SALIR" << endl;;
-			break;
-		default:
-			cout << opcion << " no es una opción válida, intente nuevamente" << endl;
+		cout << "============================================================================" << endl;
+		cout << "#                         Nunca Viste Un Torneo Así                        #" << endl;
+		cout << "#                                                                          #" << endl;
+		cout << "#              Menu                                                        #" << endl;
+		cout << "#              1. Agregar Equipo                                           #" << endl;
+		cout << "#              2. Eliminar Equipo                                          #" << endl;
+		cout << "#              3. Ver Equipos                                              #" << endl;
+		cout << "#              0. Salir                                                    #" << endl;
+		cout << "#                                                                          #" << endl;
+		cout << "#                                                            grupo114      #" << endl;
+		cout << "============================================================================" << endl;
+		cout << endl;
+		cout << " Ingrese su opción (y presione 'enter'): ";
+		cin >> opcion;
+		switch (opcion){
+			case 1:
+				cout << "Esto hará que entres en el menú de agregar equipo" << endl;
+				break;
+			case 2:
+				cout << "Esto hará que entres en el menú de eliminar equipo" << endl;
+				break;
+			case 3:
+				cout << "Esto hará que entres en el menú de ver equipos" << endl;
+				break;
+			case 0:
+				cout << "SALIR" << endl;;
+				break;
+			default:
+				cout << opcion << " no es una opción válida, intente nuevamente" << endl;
+		}
 	}
 	cout << "Gracias por usar nuestro programa!" << endl;
-	}
 
 	return 0;
 }
