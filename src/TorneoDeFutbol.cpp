4a0027cbd55ae078221b2cfcb625c87bab6eca6b
Manuel Aller <manuel.aller@globant.com>
2015-06-14 16:00:55 -0300

    modularizando y corrigiendo cosas de los contadores. Limpieza general y particular del MAIN().

--- src/TorneoDeFutbol.cpp
+++ src/TorneoDeFutbol.cpp
@@ -21,35 +21,13 @@ struct Equipo {
 namespace {
 
 const char* FILENAME = "EQUIPOS.BIN";
+const int MAX_EQUIPOS = 100;
 
 }
 
 using namespace std;
 
 
-int MostrarMenu(){
-	// muestra el menu y devuelve la opcion seleccionada
-	// Manuel
-
-	int opcion;
-	cout << " __________________________________________________________________________" << endl;
-	cout << "|                                                                          |\\" << endl;
-	cout << "|                         Torneo master race locura (TM)                   ||" << endl;
-	cout << "|                                                                          ||" << endl;
-	cout << "|              Menu                                                        ||" << endl;
-	cout << "|              1. Agregar Equipo                                           ||" << endl;
-	cout << "|              2. Eliminar Equipo                                          ||" << endl;
-	cout << "|              3. Ver Equipos                                              ||" << endl;
-	cout << "|              0. Salir                                                    ||" << endl;
-	cout << "|                                                                          ||" << endl;
-	cout << "|                                                            grupo114      ||" << endl;
-	cout << "|__________________________________________________________________________||" << endl;
-	cout << " \\_________________________________________________________________________\\|" << endl;
-	cout << endl;
-	cout << " Ingrese su opcion (y presione 'enter'): ";
-	cin >> opcion;
-	return opcion;
-}
 
 void ModificarEquipo(char id[4]){
 	// este metodo busca el equipo por el id que recibe, y permite modificarlo
@@ -58,82 +36,105 @@ void ModificarEquipo(char id[4]){
 	return;
 }
 
-void VerEquipos(int lineas,Equipo equipo[]){
-	// muestra los equipos del archivo
+void VerEquipos(Equipo equipos[],int lineas){
+	// muestra los equipos del array
 	//
 	// Manuel
 	int i;
 	cout << "Lista de equipos:" << endl;
 	for(i=0;i<lineas;i++){
-		cout << equipo[i].id << " " << equipo[i].nombre << " (" << equipo[i].potenciaAtaque << " - " << equipo[i].potenciaDefensa << ")" << endl;
+		cout << "i: " << i << " -> " << equipos[i].id << " " << equipos[i].nombre << " (" << equipos[i].potenciaAtaque << " - " << equipos[i].potenciaDefensa << ")" << endl;
 	}
 	return;
 }
 
-void AgregarEquipo(int &lineas, Equipo equipo[]){
-	int i;
-	bool agregar=true;
+void AgregarEquipo(Equipo equipos[],int &lineas){
 	Equipo nuevoEquipo;
+	string nombre; // variable auxiliar para leer una línea completa
+
 	cout << "Ingrese el identificador (3 caracteres): ";
 	cin >> nuevoEquipo.id;
-	for(i=0;i<lineas;i++){
-		if(strcmp(equipo[i].id, nuevoEquipo.id) == 0)
-		{
-			agregar=false;
-		}
-	}
-	if(agregar){
-		cout << "Ingrese el nombre (31 caracteres): ";
-/* por alguna razon, esta linea de abajo solo toma la primer palabra, hasta el espacio. */
-		cin >> nuevoEquipo.nombre;
-		cout << "Ingrese potencia de ataque (0-100): ";
-		cin >> nuevoEquipo.potenciaAtaque;
-		cout << "Ingrese potencia de defensa (0-100): ";
-		cin >> nuevoEquipo.potenciaDefensa;
+	cout << "Ingrese el nombre (31 caracteres): ";
+
+	getline(cin, nombre); // lee una línea vacía que quedó pendiente del último ingreso.
+	getline(cin, nombre); // lee una línea desde el teclado
+	strncpy(nuevoEquipo.nombre, nombre.c_str(), sizeof(nuevoEquipo.nombre)); // copia los caracteres al registro
+	nuevoEquipo.nombre[sizeof(nuevoEquipo.nombre) - 1] = '\0'; // fuerza a que tenga a lo sumo 31 caracteres
+
+	cout << "Ingrese potencia de ataque (0-100): ";
+	cin >> nuevoEquipo.potenciaAtaque;
+	cout << "Ingrese potencia de defensa (0-100): ";
+	cin >> nuevoEquipo.potenciaDefensa;
+	equipos[lineas]=nuevoEquipo;
+	lineas++;
+	return;
+}
+
+void CargarEquipos(Equipo equipos[], int &lineas){
+	/* abro el archivo al principio, cargo los equipos en un array y cierro el archivo. Trabajamos con los datos en memoria. */
+	int i;
+
+	FILE* archivo = fopen(FILENAME, "rb");
+	fread(&equipos[lineas],sizeof(equipos),1,archivo);
+	while(!feof(archivo)){
 		lineas++;
-		equipo[lineas]=nuevoEquipo;
-	} else {
-		cout << "Debo modificar!" << endl;
-		ModificarEquipo(nuevoEquipo.id);
+		fread(&equipos[lineas],sizeof(equipos),1,archivo);
+	}
+	fclose(archivo);
+/* Sólo con fines de debug, muestro lo que cargue del archivo */
+	for(i=0;i<lineas;i++){
+		cout << equipos[i].id << " " << equipos[i].nombre << " (" << equipos[i].potenciaAtaque << " - " << equipos[i].potenciaDefensa << ")" << endl;
 	}
 	return;
 }
 
-int main() {
-	// funcion Main.
+void GuardarEquipos(Equipo equipos[], int lineas){
+	int i;
+
+	FILE* archivo = fopen(FILENAME, "wb");
+	/* ahora guardo equipos en el archivo: */
+
+	for(i=0;i<lineas;i++){
+		cout << "Guardando: " << equipos[i].id << "|" << equipos[i].nombre << "|" << equipos[i].potenciaAtaque << "|" << equipos[i].potenciaDefensa << endl;
+		fwrite(&equipos[i], sizeof(equipos), 1, archivo);
+	}
+	// fwrite(&equipos, sizeof(equipos), 1, archivo);
+	fclose(archivo);
+	return;
+}
+
+
+void MenuPrincipal(Equipo equipos[MAX_EQUIPOS], int &lineas){
+	// muestra el menu y devuelve la opcion seleccionada
 	// Manuel
 
-	/* abro el archivo al principio, cargo los equipos en un array y cierro el archivo. Trabajamos con los datos en memoria. */
-	Equipo equipo[100];
-	int i;
-	int lineas=0;
-
-	FILE* archivo = fopen(FILENAME, "rb");
-	fread(&equipo[lineas],sizeof(equipo),1,archivo);
-	lineas++;
-	while(!feof(archivo)){
-		fread(&equipo[lineas],sizeof(equipo),1,archivo);
-		lineas++;
-	}
-	fclose(archivo);
-/* muestro lo que cargue del archivo */
-	for(i=0;i<lineas;i++){
-		cout << equipo[i].id << " " << equipo[i].nombre << " (" << equipo[i].potenciaAtaque << " - " << equipo[i].potenciaDefensa << ")" << endl;
-	}
-
-
-	int opcion=1;
+	int opcion=9;
 	while(opcion!=0){
-		opcion=MostrarMenu();
+		cout << " __________________________________________________________________________" << endl;
+		cout << "|                                                                          |\\" << endl;
+		cout << "|                         Torneo master race locura (TM)                   ||" << endl;
+		cout << "|                                                                          ||" << endl;
+		cout << "|              Menu                                                        ||" << endl;
+		cout << "|              1. Agregar Equipo                                           ||" << endl;
+		cout << "|              2. Eliminar Equipo                                          ||" << endl;
+		cout << "|              3. Ver Equipos                                              ||" << endl;
+		cout << "|              0. Salir                                                    ||" << endl;
+		cout << "|                                                                          ||" << endl;
+		cout << "|                                                            grupo114      ||" << endl;
+		cout << "|__________________________________________________________________________||" << endl;
+		cout << " \\_________________________________________________________________________\\|" << endl;
+		cout << endl;
+		cout << " Ingrese su opcion (y presione 'enter'): ";
+		cin >> opcion;
 		switch (opcion){
 			case 1:
-				AgregarEquipo(lineas,equipo);
+				AgregarEquipo(equipos,lineas);
 				break;
 			case 2:
 				cout << "Esto hara que entres en el menu de eliminar equipo Y LO VA A HACER IAN" << endl;
 				break;
 			case 3:
-				VerEquipos(lineas,equipo);
+				VerEquipos(equipos,lineas);
 				break;
 			case 0:
 				cout << "SALIR" << endl;;
@@ -142,13 +143,18 @@ int main() {
 				cout << opcion << " no es una opción valida, intente nuevamente" << endl;
 		}
 	}
+	return;
+}
 
-	/* ahora guardo todo en el archivo: */
-	archivo = fopen(FILENAME,"wb");
-	for(i=0;i<lineas;i++){
-		fwrite(&equipo[i], sizeof(equipo), 1, archivo);
-	}
-	fclose(archivo);
+int main() {
+	// funcion Main.
+	// Manuel
+	Equipo equipos[MAX_EQUIPOS];
+	int lineas=0;
+	CargarEquipos(equipos, lineas);
+	MenuPrincipal(equipos, lineas);
+	cout << "tengo " << lineas << endl;
+	GuardarEquipos(equipos, lineas);
 
 	cout << "Gracias por usar nuestro programa!" << endl;
 
